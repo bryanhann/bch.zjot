94f69d9d8bb05d452d984d1a2f185ed3c35263c1
Bryan Hann <nobody@nowhere>
2020-03-29 20:30:29 +0800

    Untether from config files; fix scripts for zsh

--- lib/marco.sh
+++ lib/marco.sh
@@ -1,31 +1,31 @@
 function now ()   { echo `date "+%Y-%m-%d-T-%H:%M:%S"` ; }
 function _set2 () { export $1=$2 ; }
-function _set ()  { _set2 $1 $2 && [ "$2" == "" ] && return 0 || return 1 ; }
+function _set ()  { _set2 $1 $2 && [[ "$2" == "" ]] && return 0 || return 1 ; }
 function die ()   { export fatality=$1; }
-function die_ ()  { [ . == . ]; }
+function die_ ()  { [[ . == . ]]; }
 function die_config () { die_config_heredoc ; return 98; }
 
-function die_config_heredoc () { cat << HERE 
+function die_config_heredoc () { cat << HERE
 
 zjot.sh: Environment Exception.
 Check that the following environment variables
-	ZJOT_CONFIG_ROOT 
+	ZJOT_CONFIG_ROOT
 	ZJOT_CONFIG_NAME
 are set by the configuration file ~/.config/zjot/source.
 HERE
-} 
+}
 
-function heredoc_install () { cat << HERE 
+function heredoc_install () { cat << HERE
 
 Installation note:
-    You can invoke this as a bash function by entering 
-    'source path/to/zjot.sh' at the bash prompt, or better 
-    yet add this line to your ~/.bashrc file by entering 
+    You can invoke this as a bash function by entering
+    'source path/to/zjot.sh' at the bash prompt, or better
+    yet add this line to your ~/.bashrc file by entering
     ./zjot.sh --install (or doing it by hand). Note that if
     installed to ~/.bashrc it will have to be reinstalled
     if moved.
 HERE
-} 
+}
 
 
 
--- zjot.sh
+++ zjot.sh
@@ -1,52 +1,52 @@
+source $(dirname $(grealpath $0))/lib/marco.sh
+
 function zjot() {
+    [[ -z ${ZJOT_ACC} ]] && {
+        echo "Fatal error: '\$ZJOT_ACC' is unset."
+        return -1
+    }
+
+    ACC=${ZJOT_ACC}
+    touch $ACC
+
 	[ -f $0 ] && MODE=script || MODE=function
-	[ -f $0 ] && THIS=$0     || THIS=zjot
-	source ~/.local/etc/zjot/marco.source
-	source ~/.config/zjot/source
-	_set ROOT $ZJOT_CONFIG_ROOT && die config && return 
-	_set NAME $ZJOT_CONFIG_NAME && die config && return 
 
-	ACC=`find $ROOT -name $NAME`
-	
 	case .$1 in
-		.cat)  
-			cat $ACC 
+		.cat)
+			cat $ACC
 			;;
-
-		.norm) 
-			cat $ACC | sed 's!\([:T0-9\-]*\)*!& [zjot]!'  
+		.norm)
+			cat $ACC | sed 's!\([:T0-9\-]*\)*!& [zjot]!'
 			;;
-		.new)  
+		.new)
 			tail $ACC
 			echo -------------------------------------
 			echo -n \>
 			read LINE
-			[ "$LINE" == "" ] || echo `now` "$LINE" >> $ACC
+			[[ "$LINE" == "" ]] || echo `now` "$LINE" >> $ACC
 			tail $ACC
 			echo -------------------------------------
 			echo log written at $ACC
 			;;
-		.*) 
+		.*)
 			echo "zjot -- a note taking and retrieval system."
 			echo "usage:"
-			echo "    $THIS cat"
-			echo "    $THIS norm"
-			echo "    $THIS new"
-			echo "    $THIS --install"
-			[ "$MODE" == "script" ] && heredoc_install 
+			echo "    zjot cat"
+			echo "    zjot norm"
+			echo "    zjot new"
 			;;
 	esac
-	die_${fatality} ; 
+	die_${fatality} ;
 	return $?
 }
 
 
-if [ -f $0 ]; then
-	A=~/.local/etc/zjot
-	mkdir -p                          ${A}
-	cp $0                             ${A}/zjot.source
-	cp `dirname $0`/lib/marco.source  ${A}/marco.source
-	echo source ${A}/zjot.source    > ${A}/main.source
-	cat ~/.bashrc | grep zjot\/main.source || echo source ${A}/main.source   >> ~/.bashrc
-        zjot $* 
-fi
+#if [ -f $0 ]; then
+#	A=~/.local/etc/zjot
+#	mkdir -p                          ${A}
+#	cp $0                             ${A}/zjot.source
+#	cp `dirname $0`/lib/marco.source  ${A}/marco.source
+#	echo source ${A}/zjot.source    > ${A}/main.source
+#	cat ~/.bashrc | grep zjot\/main.source || echo source ${A}/main.source   >> ~/.bashrc
+#        zjot $*
+#fi
